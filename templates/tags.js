dc345d821f1b089887f21d7573ebccd869794d3c
mylanng <mylannguyen0421@gmail.com>
2024-04-06 21:45:09 -0700

    updated

--- templates/tags.js
+++ templates/tags.js
@@ -5,25 +5,3 @@ const tags = document.querySelectorAll('.tag');
                 tag.classList.toggle('selected');
             });
         });
-
-// Get the form element by its ID
-var form = document.getElementById('bg-white rounded-lg shadow-md p-4');
-
-// Add a submit event listener to the form
-form.addEventListener('submit', function(event) {
-  // Prevent the default form submission behavior
-  event.preventDefault();
-  
-  // Get the selected tags
-  var selectedTags = [];
-  document.querySelectorAll('.tag.selected').forEach(function(selectedTag) {
-    selectedTags.push(selectedTag.getAttribute('data-tag'));
-  });
-  
-  // Set the selected tags as the value of the hidden input field
-  document.getElementById('tags').value = selectedTags.join(',');
-  
-  // Optionally, submit the form data to a server or perform other actions
-  console.log('Selected Tags:', selectedTags);
-
-});
